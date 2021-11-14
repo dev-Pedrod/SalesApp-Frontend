72ad1f21c4593d92264eeeac82857ed5bbc63258
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-14 11:18:08 -0300

    Mostrando dados e imagem do cliente para pagina de profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from '../interceptors/error-Interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
 
 @NgModule({
   declarations: [
@@ -31,8 +32,9 @@ import { StorageService } from '../services/storage.service';
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     CategoriaService,
     ErrorInterceptorProvider,
-    AuthService,,
-    StorageService
+    AuthService,
+    StorageService,
+    ClienteService
   ]
 })
 export class AppModule {}
