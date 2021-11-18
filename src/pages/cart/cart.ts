8bd0a1a4935b7d4675328f48b773652c4d97c095
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-18 11:48:11 -0300

    Terminando as funcionalidades do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { CartItem } from '../../models/cart-item';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produtos.dto';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CartItem } from '../../models/cart-item';
+import { Component } from '@angular/core';
 
 @IonicPage()
 @Component({
@@ -11,22 +12,18 @@ import { CartService } from '../../services/domain/cart.service';
   templateUrl: 'cart.html',
 })
 export class CartPage {
-
   items: CartItem[];
-
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public cartService: CartService,
     public produtoService: ProdutoService) {
   }
-
   ionViewDidLoad() {
     let cart = this.cartService.getCart();
     this.items = cart.items;
     this.loadImageUrls();
   }
-
   loadImageUrls() {
     for (var i=0; i<this.items.length; i++) {
       let item = this.items[i];
@@ -37,4 +34,24 @@ export class CartPage {
         error => {});
     }
   }  
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+  total() : number {
+    return this.cartService.total();
+  }  
+
+  goOn() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
 }
