4baa91cce3e72fccb3d341e49fac25bcaa790be1
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-20 11:18:04 -0300

    Fazendo upload da foto

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { StorageService } from '../../services/storage.service';
+import { API_CONFIG } from '../../config/api.config';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
-import { API_CONFIG } from '../../config/api.config';
-import { Camera, CameraOptions } from '@ionic-native/camera';
-
+import { StorageService } from '../../services/storage.service';
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-
   cliente: ClienteDTO;
   picture: string;
   cameraOn: boolean = false;
-
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
@@ -26,6 +23,10 @@ export class ProfilePage {
   }
 
   ionViewDidLoad() {
+    this.loadData();
+  }
+
+  loadData() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email) {
       this.clienteService.findByEmail(localUser.email)
@@ -41,7 +42,7 @@ export class ProfilePage {
     }
     else {
       this.navCtrl.setRoot('HomePage');
-    }
+    }    
   }
 
   getImageIfExists() {
@@ -51,22 +52,33 @@ export class ProfilePage {
     },
     error => {});
   }
-
   getCameraPicture() {
-
     this.cameraOn = true;
-
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.PNG,
       mediaType: this.camera.MediaType.PICTURE
     }
-
+    
     this.camera.getPicture(options).then((imageData) => {
      this.picture = 'data:image/png;base64,' + imageData;
      this.cameraOn = false;
     }, (err) => {
     });
+  }
+
+  sendPicture() {
+    this.clienteService.uploadPicture(this.picture)
+      .subscribe(response => {
+        this.picture = null;
+        this.loadData();
+      },
+      error => {
+      });
+  }
+
+  cancel() {
+    this.picture = null;
   }
 }
