4697c9de6c08f43e04fda89cf7316ac7e0800a3b
dev-Pedrod <pedroolivxira@gmail.com>
2021-11-20 11:04:24 -0300

    usando cordova para tirar foto com a camera

--- src/pages/profile/profile.module.ts
+++ src/pages/profile/profile.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
+import { Camera } from '@ionic-native/camera';
 import { IonicPageModule } from 'ionic-angular';
 import { ProfilePage } from './profile';
 
 @NgModule({
   declarations: [
     ProfilePage,
+  ],
+  providers:[
+    Camera
   ],
   imports: [
     IonicPageModule.forChild(ProfilePage),
